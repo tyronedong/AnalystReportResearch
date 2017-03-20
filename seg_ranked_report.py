573fb6fc4aec67ddb50204d257b5d3e9fc3c60ce
CStzdong <dtz1994@outlook.com>
2017-03-20 09:25:44 +0800

    Add files via upload

--- seg_ranked_report.py
+++ seg_ranked_report.py
@@ -11,16 +11,20 @@ client = MongoClient('localhost', 27017)
 db = client.AnalystReport
 collection1 = db.RatedSegReport
 collection2 = db.RankedReport
+ins_collection = db.SegRankedReport
 
 for report in collection1.find({}):
     pair_report = collection2.find_one({'_id':report['_id']})
     ranked_content_list = pair_report['ranked_content']
     content =  u'。'.join(ranked_content_list)
     seg_list = pseg.cut(content)
+    new_content_list = []
     for w in seg_list:
         if w.flag == 'm':
             continue
         if w.flag == 'x':
             continue
-
-    # print seg_list
+        new_content_list.append(w.word)
+    report['seg_ranked_content'] = new_content_list
+    report.pop('ranked_content')
+    ins_collection.insert_one(report)
